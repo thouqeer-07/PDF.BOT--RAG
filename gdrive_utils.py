bcf0ab426b6b74c19c4f2848f6a418081e582d29
SYED THOUQEER AHMED A <thouqeerahmed07@gmail.com>
2025-10-08 18:15:15 +0530

    Updated

--- gdrive_utils.py
+++ gdrive_utils.py
@@ -30,7 +30,7 @@ def get_drive_service():
         else:
             flow = InstalledAppFlow.from_client_secrets_file(
                 "client_secrets.json", SCOPES)
-            creds = flow.run_local_server(port=0)
+            creds = flow.run_console()
         # Save the credentials for the next run
         with open("token.pickle", "wb") as token:
             pickle.dump(creds, token)
