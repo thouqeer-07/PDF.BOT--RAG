f577a3781fca13eea9324e0fed0d315783eca155
SYED THOUQEER AHMED A <thouqeerahmed07@gmail.com>
2025-10-16 14:25:39 +0530

    Fully Updated

--- auth.py
+++ auth.py
@@ -26,6 +26,11 @@ def img_to_base64(path):
     with open(path, "rb") as f:
         data = f.read()
     return base64.b64encode(data).decode()
+welcome = img_to_base64("assets/MYLOGO.png")
+def img_to_base64(path):
+    with open(path, "rb") as f:
+        data = f.read()
+    return base64.b64encode(data).decode()
 log_icon_base64 = img_to_base64("assets/LOGIN.png")
 # --- MongoDB user DB helper functions ---
 def get_user_by_username_or_email(identifier):
@@ -50,7 +55,12 @@ def delete_user(username):
 def login_interface():
     col1, col2, col3 = st.columns([1, 2, 1])
     with col2:
-        st.markdown("<h3 style='text-align: center;'>Welcome To PDF Chat Bot!</h3>", unsafe_allow_html=True)
+        st.markdown("""
+          <h3 style='text-align: center;'>
+          <img src="data:image/png;base64,{welcome}" width='30' style='vertical-align: middle; margin-right: 10px;'>
+          Welcome To PDF Chat Bot!
+          </h3>
+          """, unsafe_allow_html=True)
         st.markdown("<h4 style='text-align: center;'>Login</h4>", unsafe_allow_html=True)
 
         if st.session_state.get("account_created", False):
